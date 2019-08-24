a1c2fc1ec7cdd704f7c94b7cd6d8fee5a91d25d3
unknown <kasixlv@gmail.com>
2019-08-24 23:51:56 +0300

    minor tweaks

---
+++ DinoGame/Assets/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    public void onPlay()
+    {
+        SceneManager.LoadScene("Other");
+    }
+    public void onCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    public void onExit()
+    {
+        Application.Quit();
+    }
+}
