7bae0a9fe01d44fc1550f1e53d9b6c8659325d06
Martin Viidik <martinviidik@gmail.com>
2019-08-24 13:33:58 +0300

    camera tracking

---
+++ DinoGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject target;
+
+    public float minX;
+    public float maxX;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+
+    void Update()
+    {
+
+        Vector3 cameraTransform = target.transform.position + offset;
+        transform.position = new Vector3(Mathf.Clamp(cameraTransform.x, minX, maxX), cameraTransform.y, cameraTransform.z);
+
+
+    }
+}
