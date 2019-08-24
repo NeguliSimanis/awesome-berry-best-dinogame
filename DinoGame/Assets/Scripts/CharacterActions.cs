4f81bb7367d038f04bafc03d64e0580a18290d5f
Martin Viidik <martinviidik@gmail.com>
2019-08-24 12:38:44 +0300

    Revert "Revert "Character movement""
    This reverts commit 3403082753097e0ff43b09d590cc84c29283f789.

---
+++ DinoGame/Assets/Scripts/CharacterActions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterActions : MonoBehaviour
+{
+    public string[] roars;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Roar"))
+        {
+            int i = Random.Range(0, roars.Length);
+            Debug.Log(roars[i]);
+        }
+    }
+}
