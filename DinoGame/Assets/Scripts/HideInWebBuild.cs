7c6b06e5729a2f75e79a3ebbf479369e3756d08a
NeguliSimanis <simanism@inbox.lv>
2019-09-14 14:30:37 +0300

    Added button sfx, disabled some buttons in web version

---
+++ DinoGame/Assets/Scripts/HideInWebBuild.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideInWebBuild : MonoBehaviour
+{
+    [SerializeField]
+    bool showOnlyInWeb = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        #if UNITY_WEBGL
+        if (showOnlyInWeb)
+            gameObject.SetActive(true);
+        else
+            gameObject.SetActive(false);
+
+    #else
+        if (showOnlyInWeb)
+            gameObject.SetActive(true);
+    #endif
+    }
+
+}
